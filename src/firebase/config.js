1208598eafb2d7944eb265acb718a48c29467a60
KhaiTrn <khai.tran.srp@gmail.com>
2025-02-08 11:07:28 +0700

    update 2

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -19,6 +19,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const database = getDatabase(app);
+const analytics = getAnalytics(app);
+
 export { database, ref, get, set };
