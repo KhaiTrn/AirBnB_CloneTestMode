1e3f31f8bcdd1a1e1b5aa8617f48d18ecb680183
KhaiTrn <khai.tran.srp@gmail.com>
2025-02-08 22:55:22 +0700

    update 6

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,14 +8,15 @@ import { getDatabase, ref, get, set } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  apiKey: "AIzaSyDFGWx04zeN2MCwlC9wdJcEhYch1XxY_iQ",
+  authDomain: "visitcounter-a5349.firebaseapp.com",
+  databaseURL:
+    "https://visitcounter-a5349-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "visitcounter-a5349",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  storageBucket: "visitcounter-a5349.firebasestorage.app",
+  messagingSenderId: "894432464625",
+  appId: "1:894432464625:web:fa15d05f9acdeb5fa2e350",
+  measurementId: "G-WJGDZFJB40",
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
